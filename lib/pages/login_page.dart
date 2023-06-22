91ec794a9161975e585b7628f584d70a73346eea
Rcparsln811 <receparslan811@gmail.com>
2023-06-22 12:31:58 +0300

    Loginpage düzeltme
    home page girip çıktıktan sonra boxlar temizleniyor.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,120 +7,125 @@ class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+
 bool obscurePassword = true;
+
 class _LoginPageState extends State<LoginPage> {
-  String username = "";
-  String password = "";
-  final _formkey = GlobalKey<FormState>();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Center(
-            child: Text('Login Page')),
+        title: Center(child: Text('Login Page')),
       ),
       body: Form(
-          key: _formkey,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  //autovalidateMode: AutovalidateMode.always,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    labelText: "Kullanıcı Adı",
-                    labelStyle: TextStyle(color: Colors.blue),
-                    border: OutlineInputBorder(),
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
                   ),
-                  validator: (value) {
-                    if (value != null && value.isEmpty) {
-                      return "Kullanıcı adını giriniz!";
-                    } else {
-                      return null;
-                    }
-                  },
-                  onSaved: (value) {
-                    username = value!;
-                  },
+                  labelText: "Kullanıcı Adı",
+                  labelStyle: TextStyle(color: Colors.blue),
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(
-                  height: 10.0,
-                ),
-
-                TextFormField(
-                  obscureText: obscurePassword,
-                  //autovalidateMode: AutovalidateMode.always,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    labelText: "Şifre",
-                    labelStyle: TextStyle(color: Colors.blue),
-                    border: OutlineInputBorder(),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          obscurePassword = !obscurePassword; // Şifre görünürlüğünü değiştir
-                        });
-                      },
-                      child: Icon(
-                        obscurePassword ? Icons.visibility_off : Icons.visibility,
-                      ),
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Kullanıcı adını giriniz!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: obscurePassword,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  labelText: "Şifre",
+                  labelStyle: TextStyle(color: Colors.blue),
+                  border: OutlineInputBorder(),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        obscurePassword = !obscurePassword;
+                      });
+                    },
+                    child: Icon(
+                      obscurePassword ? Icons.visibility_off : Icons.visibility,
                     ),
                   ),
-                  validator: (value) {
-                    if (value != null && value.isEmpty) {
-                      return "Şifrenizi giriniz!";
-                    } else {
-                      return null;
-                    }
-                  },
-                  onSaved: (value) {
-                    password = value!;
-                  },
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    MaterialButton(
-                      child: Text("Üye Ol"),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => RegisterPage()));
-                      },
-                    ),
-                    MaterialButton(
-                      child: Text("Şifremi Unuttum"),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => SifrePage()));
-                      },
-                    ),
-                  ],
-                ),
-                _loginButton(
-                )
-              ],
-            ),
-          )),
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Şifrenizi giriniz!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  MaterialButton(
+                    child: Text("Üye Ol"),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
+                      );
+                    },
+                  ),
+                  MaterialButton(
+                    child: Text("Şifremi Unuttum"),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => SifrePage()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+              _loginButton(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   Widget _loginButton() => ElevatedButton(
-        onPressed: () {
-          if (_formkey.currentState!.validate())
-          {_formkey.currentState!.save();
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()));
-          }
-        },
-        child: Text("Giriş Yap"),
+    onPressed: () {
+      if (_formKey.currentState!.validate()) {
+        String username = usernameController.text;
+        String password = passwordController.text;
+        // Burada giriş işlemlerini yapabilirsiniz
 
-      );
+        // Giriş işleminden sonra kullanıcı adı ve şifre alanlarını temizle
+        usernameController.clear();
+        passwordController.clear();
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
+    },
+    child: Text("Giriş Yap"),
+  );
 }
+
