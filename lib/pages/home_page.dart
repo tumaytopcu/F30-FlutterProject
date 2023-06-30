c534627041c0db45db4c4076dbd521861cb84833
Tümay Topçu <tumaytopcu.official@gmail.com>
2023-06-30 18:38:10 +0300

    firebase deneme

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,11 @@
+<<<<<<< Updated upstream
 import 'package:f30_bootcamp/pages/ana_ekran.dart';
 import 'package:f30_bootcamp/pages/profile_page.dart';
+=======
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:f30_bootcamp/auth.dart';
+>>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,11 +15,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 5;
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   final List<Widget> _pages = [
     FirstPage(),
     SecondPage(),
-    SizedBox.shrink(), // Boş bir widget
+    SizedBox.shrink(), // Empty widget
     ThirdPage(),
     FourthPage(),
     FifthPage(),
@@ -21,32 +28,59 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: _pages[_currentIndex],
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _changePage(5);
-        },
-        child: Icon(Icons.home),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8.0,
-        child: Container(
-          height: 56.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                icon: Icon(Icons.euro_symbol_outlined),
-                onPressed: () {
-                  _changePage(0);
-                },
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+            ),
+            body: _pages[_currentIndex],
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                _changePage(5);
+              },
+              child: Icon(Icons.home),
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            bottomNavigationBar: BottomAppBar(
+              shape: CircularNotchedRectangle(),
+              notchMargin: 8.0,
+              child: Container(
+                height: 56.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.euro_symbol_outlined),
+                      onPressed: () {
+                        _changePage(0);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add_location_outlined),
+                      onPressed: () {
+                        _changePage(1);
+                      },
+                    ),
+                    SizedBox(), // Empty widget
+                    IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: () {
+                        _changePage(3);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.settings_outlined),
+                      onPressed: () {
+                        _changePage(4);
+                      },
+                    ),
+                  ],
+                ),
               ),
+<<<<<<< Updated upstream
               IconButton(
                 icon: Icon(Icons.add_location_outlined),
                 onPressed: () {
@@ -70,6 +104,18 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+=======
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error initializing Firebase'));
+        }
+
+        return Center(child: CircularProgressIndicator());
+      },
+>>>>>>> Stashed changes
     );
   }
 
