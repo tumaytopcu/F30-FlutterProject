88f8c02b454168c644600cf1048fc4199568d6f7
aydnlramazan <106530954+aydnlramazan@users.noreply.github.com>
2023-07-10 14:30:35 +0300

    github desktop test committi

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //test commiti
+      //github test commiti
       debugShowCheckedModeBanner: false,
       title: 'F30',
       theme: ThemeData(
